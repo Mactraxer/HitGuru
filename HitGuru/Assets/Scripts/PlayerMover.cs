645b99ca021a1e14ebac27b55c30c3aea9c40f5a
Antonij <yesoryesuslikeass@gmail.com>
2022-06-10 16:10:13 +0300

    playerMove
    Added NavMesh for moving player
    Added script that managing player move by waypoints

---
+++ HitGuru/Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMover : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent _agent;
+    [SerializeField] private Transform[] _wayPoints;
+    private int _currentWayPoint;
+
+    private void Start()
+    {
+        _currentWayPoint = 0;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            MoveNextPoint();
+        }
+        
+    }
+
+    private void MoveNextPoint()
+    {
+        if (_currentWayPoint >= _wayPoints.Length) return;
+
+        _agent.SetDestination(_wayPoints[_currentWayPoint].position);
+        _currentWayPoint++;
+    }
+}
